59eb7f7cdfd6551946dc360ade220bd02b49b250
mug896 <mug896@gmail.com>
2019-02-12 22:05:19 +0900

    update

--- virtualbox-bash-completion.sh
+++ virtualbox-bash-completion.sh
@@ -23,7 +23,7 @@ _vboxmanage_else_words()
 
 _vboxmanage_snapname()
 {
-    WORDS=$( vboxmanage snapshot "${COMP_WORDS[2]:1:-1}" list | awk '{a[i++]=$0} END{ 
+    WORDS=$( $COM1 snapshot "${COMP_WORDS[2]:1:-1}" list | awk '{a[i++]=$0} END{ 
             if (isarray(a))
                 for (i in a) printf "%0*d) %s\n", length(i), i, a[i]
             }')
@@ -34,7 +34,7 @@ _vboxmanage_snapname()
 
 _vboxmanage_vmname()
 {
-    WORDS=$( vboxmanage list vms | awk '{a[i++]=$0} END{ 
+    WORDS=$( $COM1 list vms | awk '{a[i++]=$0} END{ 
             if (isarray(a))
                 for (i in a) printf "%0*d) %s\n", length(i), i, a[i]
             }')
@@ -48,10 +48,10 @@ _vboxmanage_double_quotes()
     if [[ $COMP_CWORD -eq 4 \
         && ( $PREV = --snapshot || $subCommandRaw =~ ${PREV}[^\ ]*" <uuid|snapname>" ) ]]
     then
-        WORDS=$( vboxmanage snapshot "${COMP_WORDS[2]:1:-1}" list \
+        WORDS=$( $COM1 snapshot "${COMP_WORDS[2]:1:-1}" list \
             | sed -r 's/.*Name: (.*) \(UUID.*/\\"\1\\"/' )
     else
-        WORDS=$( vboxmanage list vms | sed -r 's/"(.*)".*/\\"\1\\"/' )
+        WORDS=$( $COM1 list vms | sed -r 's/"(.*)".*/\\"\1\\"/' )
     fi
     IFS=$'\n'
     COMPREPLY=( $(compgen -W "$WORDS" -- \\\"$CUR) )
@@ -59,14 +59,14 @@ _vboxmanage_double_quotes()
 
 _vboxmanage_ostype()
 {
-    WORDS=$( vboxmanage list ostypes | sed -rn 's/^ID: *//p' | sort -u )
+    WORDS=$( $COM1 list ostypes | sed -rn 's/^ID: *//p' | sort -u )
     COMPREPLY=( $(compgen -W "$WORDS" -- $CUR) )
 }
 
 _vboxmanage_subcommands()
 {
 
-    WORDS=$( vboxmanage | sed '1,/Commands:/d;/Medium content access:/,$d' \
+    WORDS=$( $COM1 | sed '1,/Commands:/d;/Medium content access:/,$d' \
         | cut -d ' ' -f3 | sort -u )
     WORDS+=" mediumio extpack debugvm unattended internalcommands"
     COMPREPLY=( $(compgen -W "$WORDS" -- $CUR) )
@@ -75,7 +75,7 @@ _vboxmanage_subcommands()
 _vboxmanage_options() 
 {
     if [ $COMP_CWORD -eq 1 ]; then
-        WORDS=$( vboxmanage | sed -rn '/General Options:/,/Commands:/ s/.*(--\w+).*/\1/p; /Commands:/Q' )
+        WORDS=$( $COM1 | sed -rn '/General Options:/,/Commands:/ s/.*(--\w+).*/\1/p; /Commands:/Q' )
         COMPREPLY=( $(compgen -W "$WORDS" -- $CUR) )
     else
         WORDS=$( echo $subCommandRaw | grep -Po -- "--[\w-]+" | sort -u )
@@ -87,7 +87,7 @@ _vboxmanage()
 {
     local CUR=${COMP_WORDS[COMP_CWORD]}
     local PREV=${COMP_WORDS[COMP_CWORD-1]}
-    local COM1=VBoxManage
+    local COM1=$1
     local COM2=${COMP_WORDS[1]}
     local WORDS subCommandRaw subCommand
     local IFS=$' \t\n'
@@ -103,7 +103,7 @@ _vboxmanage()
                 internalcommands)
 
                 if [ $COMP_CWORD -eq 2 ]; then
-                    WORDS=$( vboxmanage internalcommands |& grep -Po '(?<=^  )([^ ]+)' )
+                    WORDS=$( $COM1 internalcommands |& grep -Po '(?<=^  )([^ ]+)' )
                     COMPREPLY=( $(compgen -W "$WORDS" -- $CUR) )
                     return
                 fi
@@ -112,7 +112,7 @@ _vboxmanage()
                 *)
 
                 subCommandRaw=$( $COM1 $COM2 | tail -n +3 | tr -s ' ' \
-                    | sed -r '2,${s/'"$COM1 $COM2"'//}' )
+                    | sed -r '2,${s/'"VBoxManage $COM2"'//}' )
             esac
             
         fi
