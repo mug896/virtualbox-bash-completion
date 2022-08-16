e48e132db435b3e865723fda4afe5104624b0551
Hyunho Cho <mug896@gmail.com>
2022-08-16 22:55:03 +0900

    update

--- virtualbox-bash-completion.sh
+++ virtualbox-bash-completion.sh
@@ -59,7 +59,7 @@ _vboxmanage_options()
     if [[ $1 == value ]]; then
         WORDS=$( echo $subComRaw \
             | sed -E -e ':Y s/<[^><]*>//g; tY; :Z s/\([^)(]*\)//g; tZ; s/'"VBoxManage $CMD2"'/\a/g' \
-                     -e 's/.*'"${PREV%%+([0-9])}"'[0-9]* ([^][]+).*/\1/; tX; d' \
+                     -e 's/.*'"${PREV%%+([0-9])}"'[0-9]*[= ]([^][]+).*/\1/; tX; d' \
                      -e ':X / --?[[:alnum:]]+|\a/d; s/[^[:alnum:]-]/\n/g' )
         [[ -z $WORDS ]] && { _vboxmanage_else_words; return ;}
     else 
