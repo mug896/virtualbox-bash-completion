8bf01d2cb39204e987491405ebc96f04648bef1c
Hyunho Cho <mug896@gmail.com>
2022-08-21 11:56:54 +0900

    update

--- virtualbox-bash-completion.sh
+++ virtualbox-bash-completion.sh
@@ -62,7 +62,7 @@ _vboxmanage_options()
 {
     if [[ $1 == value ]]; then
         WORDS=$( sed -E -e ':Y s/<[^><]*>//g; tY; :Z s/\([^)(]*\)//g; tZ; s/'"VBoxManage $CMD2"'/\a/g' \
-                     -e 'tR :R s/.*'"${PREV%%+([0-9])}"'[0-9]*[= ]([^][]*)].*/\1/; tX; d' \
+            -e 'tR :R s/.*'"${PREV%%+([0-9])}"'[0-9]*[= ]([^][]*]|[[:alnum:]|-]*).*/\1/; tX; d' \
                      -e ':X s/[^[:alnum:]-]/\n/g' )
     else 
         local GREP="grep -Po -- '(?<![a-z])-[[:alnum:]-]+=?'"
