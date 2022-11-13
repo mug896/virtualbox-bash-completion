958b5a88a0ae25fa54edf2d1ff93c12cc70a74a5
Hyunho Cho <mug896@gmail.com>
2022-11-13 12:19:50 +0900

    update

--- virtualbox-bash-completion.sh
+++ virtualbox-bash-completion.sh
@@ -298,7 +298,7 @@ _init_comp_wordbreaks()
 _vboxmanage() 
 {
     # It is recommended that all completion functions start with _init_comp_wordbreaks,
-    # regardless of whether you change the COMP_WORDBREAKS variable afterword.
+    # regardless of whether you change the COMP_WORDBREAKS variable afterward.
     _init_comp_wordbreaks
     [[ $COMP_WORDBREAKS != *@* ]] && COMP_WORDBREAKS+="@"
 
